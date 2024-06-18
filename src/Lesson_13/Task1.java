1ec5751e7b45a1cd05d1f833e0c153ffb0a2a1d9
Александр <al.m.morev@gmail.com>
2024-06-18 12:30:30 +0300

    Практическое задание Л13 Task1.java - добавлен второй способ подсчета дубликатов слов

--- src/Lesson_13/Task1.java
+++ src/Lesson_13/Task1.java
@@ -3,6 +3,7 @@ package Lesson_13;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
+import java.util.Collections;
 
 public class Task1 {
     public static void main(String[] args) {
@@ -24,11 +25,20 @@ public class Task1 {
         words.add("не");
         words.add("откажу");
         System.out.println("Исходный массив слов:\n" + words + "\n");
-        System.out.println("Уникальные слова в исходном массиве:\n" + new LinkedHashSet<>(words) + "\n");
+        LinkedHashSet<String> uniqueWords = new LinkedHashSet<>(words);//создание Set-а для отсечения дубликатов
+        System.out.println("Уникальные слова в исходном массиве:\n" + uniqueWords + "\n");
+        //Подсчет количества повторений слов с помощью создания вспомогательной map  с парами уникальное слово - количество повторов
         LinkedHashMap<String, Integer> duplicateOccurrences = new LinkedHashMap<>();
         for(String word : words) {
             duplicateOccurrences.put(word, duplicateOccurrences.getOrDefault(word, 0) + 1);
         }
-        System.out.println("Количество повторений каждого слова в исходном массиве:\n" + duplicateOccurrences + "\n");
+        System.out.println("Количество повторений каждого слова в исходном массиве " +
+                "(с созданием hashmap и использованием метода getOrDefault):\n" + duplicateOccurrences + "\n");
+        //Подсчет количества повторений слов с помощью метода Collections.frequency
+        System.out.println("Количество повторений каждого слова в исходном массиве " +
+                "(с использованием метода Collections.frequency):");
+        for(String word : uniqueWords){
+            System.out.print(word + "=" + Collections.frequency(words, word) + "  ");
+        }
     }
 }
