14b253bb91846edc92d836a2db104e63443ff383
Александр <al.m.morev@gmail.com>
2024-06-21 09:27:57 +0300

    Практическое задание Л14 Ветка junit - раздельные тесты по всем граничным значениям

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -1,21 +1,28 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class FactorialTest {
     @org.junit.jupiter.api.Test
     @DisplayName("Позитивный тест с аргументом 5")
     public void factorialPositiveTest(){
-        Assertions.assertTrue(Factorial.factorial(5) == 120);
+        Assertions.assertEquals(120, Factorial.factorial(5));
     }
     @org.junit.jupiter.api.Test
     @DisplayName("Проверка работы метода при нулевом аргументе")
     public void factorialZeroTest(){
-        Assertions.assertTrue(Factorial.factorial(0) == 0);
+        Assertions.assertEquals(0, Factorial.factorial(0));
     }
     @org.junit.jupiter.api.Test
     @DisplayName("Проверка работы метода при отрицательном аргументе -5")
     public void factorialNegativeTest(){
-        Assertions.assertTrue(Factorial.factorial(-5) == 0);
+        assertThrows(IllegalArgumentException.class, () -> Factorial.factorial(-1));
+    }
+    @org.junit.jupiter.api.Test
+    @DisplayName("Проверка работы метода при аргументе, дающем результат больше диапазона значений int")
+    public void factorialBigResultTest(){
+        assertThrows(IllegalArgumentException.class, () -> Factorial.factorial(13));
     }
 }
 
