3bf9df80fb4d1f69f2205df1b83be43cbb1e4d3b
Александр <al.m.morev@gmail.com>
2024-06-21 09:59:34 +0300

    Практическое задание Л14 Основная ветка - доработка метода расчета факториала

--- src/main/Factorial.java
+++ src/main/Factorial.java
@@ -1,6 +1,7 @@
 public class Factorial {
     public static int factorial(int input) {
-        if (input == 1) return 1;
+        if (input > 12 | input < 0) throw new IllegalArgumentException();
+        if (input == 1 | input == 0) return 1;
         return factorial(input - 1) * input;
     }
 }
