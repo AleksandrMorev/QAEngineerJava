a7e4e46953e8d436bd20d69cc86c05f2ea2b13e0
Александр Морев <al.m.morev@gmail.com>
2024-06-22 19:59:20 +0300

    Практическое задание Л14 Ветка junit - часть одиночных тестов заменена на параметризованные

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -1,33 +1,31 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FactorialTest {
+
     @org.junit.jupiter.api.Test
     @DisplayName("Позитивный тест с аргументом 5")
     public void factorialPositiveTest(){
         Assertions.assertEquals(120, Factorial.factorial(5));
     }
-    @org.junit.jupiter.api.Test
-    @DisplayName("Проверка работы метода при нулевом аргументе")
-    public void factorialZeroTest(){
-        Assertions.assertEquals(1, Factorial.factorial(0));
+
+    @ParameterizedTest
+    @DisplayName("Позитивный тест с двумя значениями параметров (0, 1)")
+    @ValueSource(ints = {1, 0})
+    public void factorialOneZeroTest(int parameter){
+        Assertions.assertEquals(1, Factorial.factorial(parameter));
     }
-    @org.junit.jupiter.api.Test
-    @DisplayName("Проверка работы метода при аргументе единице")
-    public void factorialOneTest(){
-        Assertions.assertEquals(1, Factorial.factorial(1));
+
+    @ParameterizedTest
+    @DisplayName("Негативный тест с двумя значениями параметров (-1, 13)")
+    @ValueSource(ints = {-1, 13})
+    public void factorialNegativeTest(int parameter){
+        assertThrows(IllegalArgumentException.class, () -> Factorial.factorial(parameter));
     }
-    @org.junit.jupiter.api.Test
-    @DisplayName("Проверка работы метода при отрицательном аргументе -1")
-    public void factorialNegativeTest(){
-        assertThrows(IllegalArgumentException.class, () -> Factorial.factorial(-1));
-    }
-    @org.junit.jupiter.api.Test
-    @DisplayName("Проверка работы метода при аргументе, дающем результат больше диапазона значений int")
-    public void factorialBigResultTest(){
-        assertThrows(IllegalArgumentException.class, () -> Factorial.factorial(13));
-    }
+
 }
 
