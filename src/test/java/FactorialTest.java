0787477e684558e6b89d72c2db66abc4f1deeaa6
Александр Морев <al.m.morev@gmail.com>
2024-06-22 23:08:19 +0300

    Практическое задание Л14 Ветка junit - добавлены теги

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -8,13 +9,15 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class FactorialTest {
 
     @org.junit.jupiter.api.Test
-    @DisplayName("Позитивный тест с аргументом 5")
+    @DisplayName("Позитивный тест с аргументом 12")
+    @Tag("smoke")
     public void factorialPositiveTest(){
-        Assertions.assertEquals(120, Factorial.factorial(5));
+        Assertions.assertEquals(479001600, Factorial.factorial(12));
     }
 
     @ParameterizedTest
     @DisplayName("Позитивный тест с двумя значениями параметров (0, 1)")
+    @Tag("sanity")
     @ValueSource(ints = {1, 0})
     public void factorialOneZeroTest(int parameter){
         Assertions.assertEquals(1, Factorial.factorial(parameter));
@@ -22,6 +25,7 @@ public class FactorialTest {
 
     @ParameterizedTest
     @DisplayName("Негативный тест с двумя значениями параметров (-1, 13)")
+    @Tag("sanity")
     @ValueSource(ints = {-1, 13})
     public void factorialNegativeTest(int parameter){
         assertThrows(IllegalArgumentException.class, () -> Factorial.factorial(parameter));
